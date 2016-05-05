e1bff142f4dfd5d0b348f2b2f886ce6bae22c4ad
Antoine Imbert <aimbert@saymedia.com>
2016-05-05 16:45:08 -0700

    More bundling unit tests

--- tests/testBundle.js
+++ tests/testBundle.js
@@ -2,7 +2,104 @@
 
 var bundler = require('../lib/bundler.js');
 
-exports.testPointer = function (test) {
+exports.testSimple = function (test) {
+
+    test.expect(3);
+
+    var schema = {
+        schemaVersion: 0,
+        '$schema': 'http://json-schema.org/draft-04/schema#',
+        type: 'object',
+        definitions: {
+            foo: {
+                type: 'string',
+            }
+        }
+    };
+
+    var b = bundler.bundle('http://test', schema, function (pointer) {});
+
+    test.ok(b);
+    test.ok(Array.isArray(b));
+    test.ok(b.length === 1);
+    test.done();
+
+};
+
+exports.testInternalRef = function (test) {
+
+    test.expect(4);
+
+    var schema = {
+        schemaVersion: 0,
+        '$schema': 'http://json-schema.org/draft-04/schema#',
+        type: 'object',
+        definitions: {
+            foo: {
+                type: 'string',
+            },
+            bar: {
+                '$ref': '#/definitions/foo'
+            }
+        }
+    };
+
+    var b = bundler.bundle('http://test', schema, function (pointer) {});
+
+    test.ok(b);
+    test.ok(Array.isArray(b));
+    test.ok(b.length === 1);
+    test.ok(b[0].definitions.bar.$ref === '#/0/definitions/foo');
+    test.done();
+
+};
+
+exports.testExternalRef = function (test) {
+
+    test.expect(4);
+
+    var schema = {
+        schemaVersion: 0,
+        '$schema': 'http://json-schema.org/draft-04/schema#',
+        type: 'object',
+        definitions: {
+            foo: {
+                type: 'string',
+            },
+            bar: {
+                '$ref': '#/definitions/foo'
+            },
+            baz: {
+                '$ref': 'http://test/A#/definitions/a'
+            }
+        }
+    };
+
+    var schemaA = {
+        schemaVersion: 0,
+        '$schema': 'http://json-schema.org/draft-04/schema#',
+        type: 'object',
+        definitions: {
+            a: {
+                type: 'string'
+            }
+        }
+    };
+
+    var b = bundler.bundle('http://test', schema, function (pointer) {
+        if (pointer.url === 'http://test/A') {
+            return schemaA;
+        }
+    });
+
+    test.ok(b);
+    test.ok(Array.isArray(b));
+    test.ok(b.length === 2);
+    test.ok(b[0].definitions.baz.$ref === '#/1/definitions/a');
+    test.done();
+};
+
+exports.testCircular = function (test) {
 
     test.expect(1);
 
