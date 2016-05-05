9924f31cef435b0f54309bf8bf1892be5d99e5bb
Antoine Imbert <aimbert@saymedia.com>
2016-05-05 16:20:26 -0700

    Basic tests for JSON-Pointer parser

--- tests/testPointer.js
+++ tests/testPointer.js
@@ -4,18 +4,26 @@ var bundler = require('../lib/bundler.js');
 
 exports.testPointer = function (test) {
 
-    test.expect(1);
+    test.expect(12);
 
     var p = new bundler.Pointer('#/foo/bar');
-    console.log(p);
+    test.ok(p.isInternal === true);
+    test.ok(p.url === '');
+    test.ok(p.fragment === '/foo/bar');
+    test.ok(p.fragmentSegments.length === 2);
 
     p = new bundler.Pointer('http://example.com');
-    console.log(p);
+    test.ok(p.isInternal === false);
+    test.ok(p.url === 'http://example.com');
+    test.ok(p.fragment === null);
+    test.ok(p.fragmentSegments === undefined);
 
     p = new bundler.Pointer('#');
-    console.log(p);
+    test.ok(p.isInternal === true);
+    test.ok(p.url === '');
+    test.ok(p.fragment === '');
+    test.ok(p.fragmentSegments.length === 0);
 
-    test.ok(1);
     test.done();
 };
 
